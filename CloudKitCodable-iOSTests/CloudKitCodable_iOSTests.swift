8690051518da7721a13e1d3dda1ffca8739da06c
Daniel Bauke <daniel.bauke@oetkerdigital.com>
2018-06-13 18:02:15 +0200

    Added iOS target

---
+++ CloudKitCodable-iOSTests/CloudKitCodable_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  CloudKitCodable_iOSTests.swift
+//  CloudKitCodable-iOSTests
+//
+//  Created by Daniel Bauke on 13.06.18.
+//  Copyright © 2018 Guilherme Rambo. All rights reserved.
+//
+
+import XCTest
+@testable import CloudKitCodable_iOS
+
+class CloudKitCodable_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
